dd0184a7c5d08741df44122a07c4cc9a420e147f
Fallenz <beijuspsn@163.com>
2023-01-17 17:20:02 +0800

    check existence before upload chunk

--- src/api/FileApi.js
+++ src/api/FileApi.js
@@ -2,9 +2,15 @@ import axios from "axios";
 
 const BASE_URL = 'http://localhost:8080/files';
 
-export const advanceUploadChunk = (formData) => axios.post(`${BASE_URL}/advance/chunk`, formData, {
+export const uploadChunk = (formData) => axios.post(`${BASE_URL}/advance/chunk`, formData, {
     headers: {
         'Content-Type': 'multipart/form-data'
     }
 })
+    .then((resp) => resp.data);
+
+export const mergeChunk = (data) => axios.post(`${BASE_URL}/advance/chunk/merge`, data)
+    .then((resp) => resp.data);
+
+export const checkFileExistence = (data) => axios.post(`${BASE_URL}/advance/check-existence`, data)
     .then((resp) => resp.data);
--- src/utils/FileUtils.js
+++ src/utils/FileUtils.js
@@ -1,5 +1,5 @@
 import SparkMD5 from 'spark-md5';
-import {advanceUploadChunk} from "../api/FileApi";
+import {uploadChunk, mergeChunk, checkFileExistence} from "../api/FileApi";
 
 const CHUNK_SIZE = 5 * 1024 * 1024; // 5mb分片
 
@@ -7,7 +7,8 @@ export const chunkUpload = (file) => {
     let fileReader = new FileReader();
     let blobSlice = File.prototype.slice || File.prototype.mozSlice || File.prototype.webkitSlice;
     let currentChunk = 0;
-    let chunks = Math.ceil(file.size / CHUNK_SIZE);
+    let size = file.size;
+    let totalChunkCount = Math.ceil(size / CHUNK_SIZE);
     let spark = new SparkMD5.ArrayBuffer();
 
     const loadNext = () => {
@@ -20,33 +21,64 @@ export const chunkUpload = (file) => {
 
     fileReader.onload = (e) => {
         spark.append(e.target.result);
-        if (currentChunk < chunks) {
+        if (currentChunk < totalChunkCount) {
             currentChunk++;
             loadNext();
         } else {
             let md5 = spark.end();
             console.log(`full file md5:${md5}`);
-            doUpload(file, md5)
+            checkFileExistence({md5})
+                .then(async (resp) => {
+                    if (!resp.fullFileExist) {
+                        await doUpload(file, md5, totalChunkCount, size, resp.validChunkNumbers)
+                        doMerge(file.name, md5, totalChunkCount, size)
+                    } else {
+                        console.log('full file already existed, upload completed')
+                    }
+
+                })
         }
     };
 
 };
 
-const doUpload = (file, md5) => {
-    let size = file.size;
-    let chunks = Math.ceil(size / CHUNK_SIZE);
-    for (let i = 0; i < chunks; i++) {
+const doUpload = async (file, md5, totalChunkCount, size, validChunkNumbers) => {
+    for (let i = 0; i < totalChunkCount; i++) {
         let chunkNumber = i + 1;
+        if (validChunkNumbers.contains(chunkNumber)) {
+            console.log('test skip chunk upload')
+            continue
+        }
         let chunkStart = (chunkNumber - 1) * CHUNK_SIZE;
         let chunkEnd = Math.min(size, chunkStart + CHUNK_SIZE);
         let slice = file.slice(chunkStart, chunkEnd);
         let formData = new FormData();
         formData.append('chunk', slice);
-        formData.append('md5', md5);
-        advanceUploadChunk(formData).then((data) => {
+        formData.append('number', chunkNumber);
+        formData.append('standardSize', CHUNK_SIZE);
+        formData.append('totalChunkCount', totalChunkCount);
+        formData.append('fullFileName', file.name);
+        formData.append('fullFileMd5', md5);
+        formData.append('currentChunkMd5', md5);
+        await uploadChunk(formData).then((data) => {
             console.log(`upload chunk successfully:${data}`);
         });
     }
+}
+
+const doMerge = (fileName, md5, totalChunkCount, fullFileSize) => {
+    mergeChunk({
+        fullFileMd5: md5,
+        fullFileName: fileName,
+        totalChunkCount,
+        fullFileSize
+    })
+        .then((data) => {
+            console.log(`merge chunk successfully:${data}`);
+        })
+        .catch((err) => {
+            console.log(`merge chunk failed:${err}`);
+        });
 }
 
 
