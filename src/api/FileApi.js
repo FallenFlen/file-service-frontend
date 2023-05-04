c5b1ba66c34de54d2b62f6579854d6053beaabd2
Fallenz <beijuspsn@163.com>
2023-05-04 16:06:30 +0800

    remove useless codes

--- src/api/FileApi.js
+++ src/api/FileApi.js
@@ -2,33 +2,18 @@ import axios from "axios";
 
 const BASE_URL = 'http://localhost:8080/files';
 
-export const uploadChunk = (formData) => axios.post(`${BASE_URL}/advance/chunk`, formData, {
+export const uploadChunk = (formData) => axios.post(`${BASE_URL}/chunk`, formData, {
     headers: {
         'Content-Type': 'multipart/form-data'
     }
 })
     .then((resp) => resp.data);
 
-export const mergeChunk = (data) => axios.post(`${BASE_URL}/advance/chunk/merge`, data)
+export const mergeChunk = (data) => axios.post(`${BASE_URL}/chunk/merge`, data)
     .then((resp) => resp.data);
 
-export const checkFileExistence = (data) => axios.post(`${BASE_URL}/advance/chunk/validate-and-clean-damaged`, data)
+export const checkFileExistence = (data) => axios.post(`${BASE_URL}/chunk/validate-and-clean-damaged`, data)
     .then((resp) => resp.data);
 
-export const findAll = () => axios.get(`${BASE_URL}/advance/all`)
+export const findAll = () => axios.get(`${BASE_URL}/all`)
     .then((resp) => resp.data);
-
-export const advanceDownload = (path, range) => axios.get(`${BASE_URL}/advance/download`, {
-    params: {
-        path
-    },
-    headers: {
-        "Range": range
-    }
-})
-
-export const commonDownload = (path) => axios.get(`${BASE_URL}/common/download`, {
-    params: {
-        path
-    }
-})
